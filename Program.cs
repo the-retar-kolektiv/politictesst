7e29b36464abc31704cca6bbc978b9d0b09e3155
Totpoter <Totpoter@users.noreply.github.com>
2019-03-18 10:53:06 +0100

    am do politic tesst but habe no kvestions jet
    need do kvestions for tesst

---
+++ Program.cs
@@ -0,0 +1,373 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+
+namespace myeh
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool running = true;
+            while(running)
+            {
+                Start:
+                int auth = 0;
+                int cap = 0;
+                int trad = 0;
+                int nat = 0;
+
+                Console.WriteLine("EPIC MEGA QUIZ TIME!\n\nPress any key to start");
+                Console.ReadKey();
+                /* start auth kvestions */
+                Console.WriteLine("\nfwomp");
+                string a1 = Console.ReadLine();
+                if(a1 == "yes")
+                {
+                    auth += 1;
+                }
+                Console.WriteLine("blep");
+                string a2 = Console.ReadLine();
+                if(a2 == "yes")
+                {
+                    auth += 1;
+                }
+                Console.WriteLine("plomp");
+                string a3 = Console.ReadLine();
+                if (a3 == "yes")
+                {
+                    auth += 1;
+                }
+                Console.WriteLine("skomp");
+                string a4 = Console.ReadLine();
+                if (a4 == "yes")
+                {
+                    auth += 1;
+                }
+                Console.WriteLine("blomp");
+                string a5 = Console.ReadLine();
+                if (a5== "yes")
+                {
+                    auth += 1;
+                }
+                Console.WriteLine("chomp");
+                string a6 = Console.ReadLine();
+                if (a6 == "yes")
+                {
+                    auth += 1;
+                }
+                Console.WriteLine("yomp");
+                string a7 = Console.ReadLine();
+                if (a7 == "yes")
+                {
+                    auth += 1;
+                }
+                Console.WriteLine("zomp");
+                string a8 = Console.ReadLine();
+                if (a8 == "yes")
+                {
+                    auth += 1;
+                }
+                Console.WriteLine("momp");
+                string a9 = Console.ReadLine();
+                if (a9 == "yes")
+                {
+                    auth += 1;
+                }
+                Console.WriteLine("klomp");
+                string a10 = Console.ReadLine();
+                if (a10 == "yes")
+                {
+                    auth += 1;
+                }
+                /* start cap kvestions */
+                Console.WriteLine("\nfwomp");
+                string a11 = Console.ReadLine();
+                if (a11 == "yes")
+                {
+                    cap += 1;
+                }
+                Console.WriteLine("blep");
+                string a12 = Console.ReadLine();
+                if (a12 == "yes")
+                {
+                    cap += 1;
+                }
+                Console.WriteLine("plomp");
+                string a13 = Console.ReadLine();
+                if (a13 == "yes")
+                {
+                    cap += 1;
+                }
+                Console.WriteLine("skomp");
+                string a14 = Console.ReadLine();
+                if (a14 == "yes")
+                {
+                    cap += 1;
+                }
+                Console.WriteLine("blomp");
+                string a15 = Console.ReadLine();
+                if (a15 == "yes")
+                {
+                    cap += 1;
+                }
+                Console.WriteLine("chomp");
+                string a16 = Console.ReadLine();
+                if (a16 == "yes")
+                {
+                    cap += 1;
+                }
+                Console.WriteLine("yomp");
+                string a17 = Console.ReadLine();
+                if (a17 == "yes")
+                {
+                    cap += 1;
+                }
+                Console.WriteLine("zomp");
+                string a18 = Console.ReadLine();
+                if (a18 == "yes")
+                {
+                    cap += 1;
+                }
+                Console.WriteLine("momp");
+                string a19 = Console.ReadLine();
+                if (a19 == "yes")
+                {
+                    cap += 1;
+                }
+                Console.WriteLine("klomp");
+                string a20 = Console.ReadLine();
+                if (a20 == "yes")
+                {
+                    cap += 1;
+                }
+                /* start trad kvestions */
+                Console.WriteLine("\nfwomp");
+                string a21 = Console.ReadLine();
+                if (a21 == "yes")
+                {
+                    nat += 1;
+                }
+                Console.WriteLine("blep");
+                string a22 = Console.ReadLine();
+                if (a22 == "yes")
+                {
+                    nat += 1;
+                }
+                Console.WriteLine("plomp");
+                string a23 = Console.ReadLine();
+                if (a23 == "yes")
+                {
+                    nat += 1;
+                }
+                Console.WriteLine("skomp");
+                string a24 = Console.ReadLine();
+                if (a24 == "yes")
+                {
+                    nat += 1;
+                }
+                Console.WriteLine("blomp");
+                string a25 = Console.ReadLine();
+                if (a25 == "yes")
+                {
+                    nat += 1;
+                }
+                Console.WriteLine("chomp");
+                string a26 = Console.ReadLine();
+                if (a26 == "yes")
+                {
+                    nat += 1;
+                }
+                Console.WriteLine("yomp");
+                string a27 = Console.ReadLine();
+                if (a27 == "yes")
+                {
+                    nat += 1;
+                }
+                Console.WriteLine("zomp");
+                string a28 = Console.ReadLine();
+                if (a28 == "yes")
+                {
+                    nat += 1;
+                }
+                Console.WriteLine("momp");
+                string a29 = Console.ReadLine();
+                if (a29 == "yes")
+                {
+                    nat += 1;
+                }
+                Console.WriteLine("klomp");
+                string a30 = Console.ReadLine();
+                if (a30 == "yes")
+                {
+                    nat += 1;
+                }
+                /* start trad kvestions */
+                Console.WriteLine("\nfwomp");
+                string a31 = Console.ReadLine();
+                if (a31 == "yes")
+                {
+                    trad += 1;
+                }
+                Console.WriteLine("blep");
+                string a32 = Console.ReadLine();
+                if (a32 == "yes")
+                {
+                    trad += 1;
+                }
+                Console.WriteLine("plomp");
+                string a33 = Console.ReadLine();
+                if (a33 == "yes")
+                {
+                    trad += 1;
+                }
+                Console.WriteLine("skomp");
+                string a34 = Console.ReadLine();
+                if (a34 == "yes")
+                {
+                    trad += 1;
+                }
+                Console.WriteLine("blomp");
+                string a35 = Console.ReadLine();
+                if (a35 == "yes")
+                {
+                    trad += 1;
+                }
+                Console.WriteLine("chomp");
+                string a36 = Console.ReadLine();
+                if (a36 == "yes")
+                {
+                    trad += 1;
+                }
+                Console.WriteLine("yomp");
+                string a37 = Console.ReadLine();
+                if (a37 == "yes")
+                {
+                    trad += 1;
+                }
+                Console.WriteLine("zomp");
+                string a38 = Console.ReadLine();
+                if (a38 == "yes")
+                {
+                    trad += 1;
+                }
+                Console.WriteLine("momp");
+                string a39 = Console.ReadLine();
+                if (a39 == "yes")
+                {
+                    trad += 1;
+                }
+                Console.WriteLine("klomp");
+                string a40 = Console.ReadLine();
+                if (a40 == "yes")
+                {
+                    trad += 1;
+                }
+                /* kalkulat ansers */
+                string authScale = "";
+                if(auth <= 2)
+                {
+                    authScale = "Anarchist";
+                }
+                if(auth <= 4 && auth > 2)
+                {
+                    authScale = "Libertarian";
+                }
+                if(auth <= 6 && auth > 4)
+                {
+                    authScale = "Moderate";
+                }
+                if(auth <= 8 && auth > 6)
+                {
+                    authScale = "Authoritarian";
+                }
+                if(auth <= 10 && auth > 8)
+                {
+                    authScale = "Totalitarian";
+                }
+                string capScale = "";
+                if (cap <= 2)
+                {
+                    capScale = "Communist";
+                }
+                if (cap <= 4 && cap > 2)
+                {
+                    capScale = "Socialist";
+                }
+                if (cap <= 6 && cap > 4)
+                {
+                    capScale = "Centrist";
+                }
+                if (cap <= 8 && cap > 6)
+                {
+                    capScale = "Capitalist";
+                }
+                if (cap <= 10 && cap > 8)
+                {
+                    capScale = "Laissez-faire";
+                }
+                string natScale = "";
+                if (nat <= 2)
+                {
+                    natScale = "Globalist";
+                }
+                if (nat <= 4 && nat > 2)
+                {
+                    natScale = "Internationalist";
+                }
+                if (nat <= 6 && nat > 4)
+                {
+                    natScale = "Balanced";
+                }
+                if (nat <= 8 && nat > 6)
+                {
+                    natScale = "Nationalist";
+                }
+                if (nat <= 10 && nat > 8)
+                {
+                    natScale = "Chauvinist";
+                }
+                string tradScale = "";
+                if (trad <= 2)
+                {
+                    tradScale = "Progressive";
+                }
+                if (trad <= 4 && trad > 2)
+                {
+                    tradScale = "TODO";
+                }
+                if (trad <= 6 && trad > 4)
+                {
+                    tradScale = "Neutral";
+                }
+                if (trad <= 8 && trad > 6)
+                {
+                    tradScale = "TODO";
+                }
+                if (trad <= 10 && trad > 8)
+                {
+                    tradScale = "Traditionalist";
+                }
+                /* shob anser */
+                Console.WriteLine("yuo be " + authScale + " " + capScale + " " + natScale + " " + tradScale + ". thank for be taek tesst\nwan pley agen?");
+                PlayAgain:
+                string again = Console.ReadLine();
+                if(again =="yes")
+                {
+                    goto Start;
+                }
+                else if(again == "no")
+                {
+                    running = false;
+                }
+                else
+                {
+                    Console.WriteLine("sori fren am not understannd :( ples do yes or no");
+                    goto PlayAgain;
+                }
+            }
+        }
+    }
+}
